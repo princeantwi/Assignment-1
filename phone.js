26c181661048cb9cab30cbff763c6bbc2d48f65c
princeantwi <pran4690@colorado.edu>
2018-01-29 19:01:09 -0700

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,9 +1,40 @@
-$(document).ready(function() { // do this when the document is loaded
- $("#element").show(); // show the element with ID "element"
- $("#otherElement").hide(); // hide the element with ID "otherElement"
+
+ $(document).ready(function() { // do this when the document is loaded
+ $("#dialer").show(); // show the element with ID "element"
+ $("#content_dialer").show(); // show element
+ $("#content_list").hide(); // hide the element with ID "otherElement"
+ $("#content_add").hide();
 });
 
-$("#button_id").click(function() { // when "button_id" is clicked
- $("#element").show(); // show element
- $("#other_element").hide(); // hide other element
-});
+//When the Dialer is clicked
+  $("#dialer").click(function() { // when "button_id" is clicked
+  $("#content_dialer").show(); // show element
+  $("#content_list").hide(); // show element
+  $("#content_add").hide(); // hide other element
+  });
+
+// When the Contact List is clicked
+  $("#contact_list").click(function() { // when "button_id" is clicked
+  $("#content_dialer").hide(); // show element
+  $("#content_list").show(); // show element
+  $("#content_add").hide(); // hide other element
+  });
+
+//When Add Contact is clicked
+  $("#add_contact").click(function() { // when "button_id" is clicked
+  $("#content_dialer").hide(); // show element
+  $("#content_list").hide(); // show element
+  $("#content_add").show(); // hide other element
+ });
+
+//Number Pad
+ var audio = $("#click")[0];
+ $("li").click(function() {
+   audio.play();
+ });
+
+ $('.keys').on('click', 'li', function() {
+   var num = $(this).data('number')
+   $("#telnumber").append(num);
+ });
+
